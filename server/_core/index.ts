f2ccddda6df3691eddb487cb08a0d26ade6bf18f
tayebkedadouche09-a11y <tayebkedadouche09@gmail.com>
2026-09-18 13:29:58 +0100

    fix: import HTTP Server type explicitly

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import express from "express";
-import { createServer } from "http";
+import { createServer, type Server } from "http";
 import net from "net";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { registerOAuthRoutes } from "./oauth";
