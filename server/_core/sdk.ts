273cf5ce1a337f490d943d630118585b5400da85
tayebkedadouche09-a11y <tayebkedadouche09@gmail.com>
2026-09-18 13:32:05 +0100

    fix: use Express core request type

--- server/_core/sdk.ts
+++ server/_core/sdk.ts
@@ -2,7 +2,7 @@ import { AXIOS_TIMEOUT_MS, COOKIE_NAME, ONE_YEAR_MS, decodeOAuthState } from "@s
 import { ForbiddenError } from "@shared/_core/errors";
 import axios, { type AxiosInstance } from "axios";
 import { parse as parseCookieHeader } from "cookie";
-import type { Request } from "express";
+import type { Request } from "express-serve-static-core";
 import { SignJWT, jwtVerify } from "jose";
 import type { User } from "../../drizzle/schema";
 import * as db from "../db";
